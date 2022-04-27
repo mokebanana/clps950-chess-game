1a359aaedd773272ad3d0d64ff58d8ca33aa4dd4
mokebanana <jiawen_li1@brown.edu>
2022-04-26 23:12:45 -0400

    Copy and pasted to set up all the pieces made some imports to start working on board

--- chessMain.py
+++ chessMain.py
@@ -1,1 +1,3 @@
-import pygame as p
+import pygame as pg
+import matplotlib as plt
+import numpy as np
--- pieces.py
+++ pieces.py
@@ -5,4 +5,43 @@ class Pawn:
         self.move = False
 
         def moveTo(self, newCoord):
-            
+
+class Bishop:
+    def __init__(self, color, coord):
+        self.color = color
+        self.coord = coord
+        self.move = False
+
+        def moveTo(self, newCoord):
+
+class King:
+    def __init__(self, color, coord):
+        self.color = color
+        self.coord = coord
+        self.move = False
+
+        def moveTo(self, newCoord):
+
+class Queen:
+    def __init__(self, color, coord):
+         self.color = color
+         self.coord = coord
+         self.move = False
+
+         def moveTo(self, newCoord):
+
+class Rook:
+    def __init__(self, color, coord):
+          self.color = color
+          self.coord = coord
+          self.move = False
+
+          def moveTo(self, newCoord):
+
+class Knight:
+    def __init__(self, color, coord):
+          self.color = color
+          self.coord = coord
+          self.move = False
+
+          def moveTo(self, newCoord):
