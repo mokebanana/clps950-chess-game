596dbc318ab3674bf0be642029f9cd1febeb4ef4
mokebanana <jiawen_li1@brown.edu>
2022-04-28 13:15:21 -0400

    Started GameState class and classes for pieces

---
+++ gameState.py
@@ -0,0 +1,8 @@
+
+"""
+Class for storing current state of game state for the chess game.
+    responsible for determining valid and invalid moves.
+"""
+class GameState():
+    def __init__(self):
+        self.board = []
---
+++ pieces.py
@@ -0,0 +1,8 @@
+class Pawn:
+    def __init__(self, color, coord):
+        self.color = color
+        self.coord = coord
+        self.move = False
+
+        def moveTo(self, newCoord):
+            
