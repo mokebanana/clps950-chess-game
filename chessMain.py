14e71e36dcd2512ab669fc012568320c35321851
mokebanana <jiawen_li1@brown.edu>
2022-05-01 00:39:40 -0400

    chessMain, gameState
    chessMain:
    - coded functions pieceGraphics, main, and displayGS
    - displayGS does not currently work, need to ask TA
    
    gameState:
    - worked on classes GameState (function movePiece) and Move
    
    Overall time worked: 6 hours

--- chessMain.py
+++ chessMain.py
@@ -1,3 +1,84 @@
-import pygame as pg
+import pygame as p
 import matplotlib as plt
+from PIL import Image
 import numpy as np
+
+# global variables for board dimensions and piece graphics
+import gameState
+
+boardLength = 800
+numSquares = 8
+squareLength = boardLength // numSquares
+imageDict = {}  # keys: piece name, val: corresponding image of piece
+
+
+def pieceGraphics():
+    """
+    Function to load piece graphics into imageDict at the beginning
+    """
+    pieceTypes = ['B', 'K', 'N', 'P', 'Q', 'R']
+    for piece in pieceTypes:
+        imageDict['w' + piece] = p.image.load("pieceImages/w" + piece + ".png")
+        imageDict['b' + piece] = p.image.load("pieceImages/b" + piece + ".png")
+
+def main():
+    """
+    Main Function
+    """
+    p.init()  # initialize pygame
+
+    screen = p.display.set_mode((boardLength, boardLength))
+    screen.fill("white")
+    pieceGraphics()
+
+    # access GameState
+    gs = gameState.GameState
+
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        displayGS(screen, gs)
+        p.display.flip()
+
+
+def displayGS(screen, gs):
+    """
+    Function that displays the board and pieces
+    """
+    # display the board
+    for row in range(numSquares):
+        if row % 2 == 0:  # even index rows
+            for column in range(numSquares):
+                if column % 2 == 0:  # even index cols = white
+                    p.draw.rect(screen, "white", p.Rect(column * squareLength, row * squareLength, squareLength,
+                                                        squareLength))
+                else:
+                    p.draw.rect(screen, "gray", p.Rect(column * squareLength, row * squareLength, squareLength,
+                                                       squareLength))
+        else:  # odd index rows
+            for column in range(numSquares):
+                if column % 2 == 0:  # even index cols = gray
+                    p.draw.rect(screen, "gray", p.Rect(column * squareLength, row * squareLength, squareLength,
+                                                       squareLength))
+                else:
+                    p.draw.rect(screen, "white", p.Rect(column * squareLength, row * squareLength, squareLength,
+                                                        squareLength))
+
+    # display the pieces
+    board = gs.board
+    for row in range(numSquares):
+        for column in range(numSquares):
+            pieceName = board[row][column]
+            if pieceName != "--":  # not empty square
+                screen.blit(imageDict[pieceName], p.Rect(column * squareLength, row * squareLength, squareLength,
+                                                       squareLength))
+
+    """
+    TODO: gs.board is not currently working, I am getting an error that says:
+        "AttributeError: type object 'GameState' has no attribute 'board'
+    
+    I have no idea why this is and it is driving me crazy!! - Angela
+    """
+main()
--- gameState.py
+++ gameState.py
@@ -1,8 +1,60 @@
-
 """
 Class for storing current state of game state for the chess game.
     responsible for determining valid and invalid moves.
 """
-class GameState():
+
+
+class GameState:
     def __init__(self):
-        self.board = []
+        """
+        The chess board is represented by 8x8 2d list
+        Each character is represented by a 2-letter str.
+            1st char: color ('b', 'w')
+            2nd char: piece type ('R', 'N', 'B', 'Q', 'K' or 'p')
+            empty space: '--'
+
+            (character notations inspired by GitHub user mikolaj-skrzypczak)
+        """
+        self.board = [["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+                      ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+                      ["--", "--", "--", "--", "--", "--", "--", "--"],
+                      ["--", "--", "--", "--", "--", "--", "--", "--"],
+                      ["--", "--", "--", "--", "--", "--", "--", "--"],
+                      ["--", "--", "--", "--", "--", "--", "--", "--"],
+                      ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+                      ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+
+        self.whiteMoveNext = True  # white moves first
+        self.checkMate = False
+
+        # TODO: initialize pawn promotion, enpassant, castling, etc. conditions here
+
+        def movePiece(self, move):
+            """
+            Function to make a move
+            :param self:
+            :param move:
+            :return:
+            """
+            self.board[move.startRow][move.startCol] = "--"
+
+
+"""
+Class for moving pieces from start to end square
+    responsible for 
+"""
+
+
+class Move:
+    def __init__(self, board, start, end):
+        # store coordinates of start and end points of piece
+        self.startR = start[0]
+        self.startC = start[1]
+        self.endR = end[0]
+        self.endC = end[1]
+
+        # origin square of piece [first click]
+        self.startSquare = board[self.startR][self.startC]
+
+        # target square of piece [second click]
+        self.endSquare = board[self.endR][self.endC]
