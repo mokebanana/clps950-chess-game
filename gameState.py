207de3f2324ebfb2d744b5fc35c919db17c253fc
ynajmanl <101124174+ynajmanl@users.noreply.github.com>
2022-05-02 10:03:12 -0400

    Copy and pasted to set up all the pieces made some imports to start working on board

--- gameState.py
+++ gameState.py
@@ -5,7 +5,7 @@ Class for storing current state of game state for the chess game.
 
 
 class GameState:
-    def __init__(self):
+    def __init__(self, board):
         """
         The chess board is represented by 8x8 2d list
         Each character is represented by a 2-letter str.
@@ -43,6 +43,7 @@ class GameState:
 Class for moving pieces from start to end square
     responsible for 
 """
+    def makeMove(self, move)
 
 
 class Move:
@@ -58,3 +59,15 @@ class Move:
 
         # target square of piece [second click]
         self.endSquare = board[self.endR][self.endC]
+    #change matrix to chess notation
+    ranksToRows = {"1":7, "2":6, "3":5, "4":4, "5":3, "6":2, "7":1, "8":0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+    filesToColumns = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+    columnsToFiles = {v: k for k, v in filesToColumns.items()}
+    #print chess notation for move
+    def chessNotation(self):
+        return self.getRankFile(self.startR, self.startC) + self.getRankFile(self.endR, self.endC)
+    def RankAndFile(self, r, c):
+        return self.columnsToFiles[c] + self.rowsToRanks[r]
+
+
--- pieces.py
+++ pieces.py
@@ -1,47 +1,53 @@
 class Pawn:
-    def __init__(self, color, coord):
+    def __init__(self, color, coord, onoff):
         self.color = color
         self.coord = coord
         self.move = False
+        self.onoff = onoff
 
-        def moveTo(self, newCoord):
+        def move_to(self, new_coord):
 
 class Bishop:
-    def __init__(self, color, coord):
+    def __init__(self, color, coord, onoff):
         self.color = color
         self.coord = coord
         self.move = False
+        self.onoff = onoff
 
-        def moveTo(self, newCoord):
+        def move_to(self, new_coord):
 
 class King:
-    def __init__(self, color, coord):
+    def __init__(self, color, coord, onoff):
         self.color = color
         self.coord = coord
         self.move = False
+        self.onoff = onoff
 
-        def moveTo(self, newCoord):
+        def move_to(self, new_coord):
 
 class Queen:
-    def __init__(self, color, coord):
+    def __init__(self, color, coord, onoff):
          self.color = color
          self.coord = coord
          self.move = False
+         self.onoff = onoff
 
-         def moveTo(self, newCoord):
+         def move_to(self, new_coord):
 
 class Rook:
-    def __init__(self, color, coord):
+    def __init__(self, color, coord, onoff):
           self.color = color
           self.coord = coord
           self.move = False
+          self.onoff = onoff
 
-          def moveTo(self, newCoord):
+          def move_to(self, new_coord):
 
 class Knight:
-    def __init__(self, color, coord):
+    def __init__(self, color, coord, onoff):
           self.color = color
           self.coord = coord
           self.move = False
+          self.onoff = onoff
 
-          def moveTo(self, newCoord):
+    def move_to(self, new_coord):
